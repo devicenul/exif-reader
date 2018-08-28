15bce7d97ab0bbcd6f0dfdaf2ed0e5893d07eec0
rjefferson <rich.jefferson@sensus.com>
2018-08-28 15:40:06 -0400

    Create basic framework.

---
+++ src/main/java/net/mercnet/exifreader/ExifReaderApplication.java
@@ -0,0 +1,12 @@
+package net.mercnet.exifreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExifReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExifReaderApplication.class, args);
+	}
+}
